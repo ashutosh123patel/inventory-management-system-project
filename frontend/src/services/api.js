8c29f12f786ab622f32d8e6921459cdca7b96ef3
ashutosh123patel <ashutoshp141@gmail.com>
2026-04-17 22:51:17 +0530

    Updated package-lock.json

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = process.env.REACT_APP_API_URL || "http://localhost:5000/api/v1";
+const API_URL = process.env.REACT_APP_API_URL || "https://inventory-management-system-miniproject.onrender.com/api/v1";
 
 const API = axios.create({
   baseURL: API_URL,
